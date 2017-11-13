2b593598282062227e320d5739f51e44a23457a5
CarlosMarcel <carlos.strike.quesada@gmail.com>
2017-11-13 02:48:38 -0600

    Commit #10
    R-Liquidez terminado!

--- src/app/Componentes/rliquidez/rliquidez.component.ts
+++ src/app/Componentes/rliquidez/rliquidez.component.ts
@@ -134,17 +134,174 @@ export class RLiquidezComponent implements OnInit {
           this.global.saldo_cxc_final_P1 = this.global.saldo_cxc - this.global.saldo_cxc_PeriodoFinalP1 + this.global.abono_cxc_PeriodoFinalP1;
 
           this.global.activoCorrienteP1 = this.global.efectivoP1 + this.global.inventarioFinalP1 + this.global.saldo_cxc_final_P1;
+
+          this.global.rotacionCarteraP1 = this.global.saldo_cxc_P1/(this.global.saldo_cxc_inicial_P1+this.global.saldo_cxc_final_P1)/2;
         });
 
       //---CXP INICIAL
+      this.service.get_cxp_periodo(this.fechaInicioP1,this.fechaActual).subscribe(
+        rs => {this.global.saldo_cxp_PeriodoInicialP1 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+
+      this.service.get_cxp_abonos_periodo(this.fechaInicioP1,this.fechaActual).subscribe(
+        rs => {this.global.abono_cxp_PeriodoInicialP1 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          this.global.saldo_cxp_inicial_P1 = this.global.saldo_cxp - this.global.saldo_cxp_PeriodoInicialP1 + this.global.abono_cxp_PeriodoInicialP1;
+        });
       //---CXP FINAL
+      this.service.get_cxp_periodo(this.fechaFinP1,this.fechaActual).subscribe(
+        rs => {this.global.saldo_cxp_PeriodoFinalP1 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
 
+      this.service.get_cxp_abonos_periodo(this.fechaFinP1,this.fechaActual).subscribe(
+        rs => {this.global.abono_cxp_PeriodoFinalP1 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          this.global.saldo_cxp_final_P1 = this.global.saldo_cxp - this.global.saldo_cxp_PeriodoFinalP1 + this.global.abono_cxp_PeriodoFinalP1;
 
+          this.global.pasivoCorrienteP1 = this.global.saldo_cxp_final_P1;
 
+          //Variables de la vista
+          this.global.capitalNetoP1 = this.global.activoCorrienteP1 - this.global.pasivoCorrienteP1;
+
+          this.global.indiceSolvenciaP1 = this.global.activoCorrienteP1 / this.global.pasivoCorrienteP1;
+
+          this.global.pruebaAcidaP1 = (this.global.activoCorrienteP1 - this.global.inventario)/this.global.pasivoCorrienteP1;
+
+          this.global.rotacionCXP_P1 = this.global.saldo_cxp_P1 / (this.global.saldo_cxp_inicial_P1+this.global.saldo_cxp_final_P1)/2;
+        });
     }
 
     if(this.global.verPeriodo2){
+      //Funciones sin dependencia de otras.
+      //Inventario
+      this.service.getInventario().subscribe(
+        rs => {this.global.inventario = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //SALDO CXC
+      this.service.getSaldo_CXC().subscribe(
+        rs => {this.global.saldo_cxc = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //SALDO CXP
+      this.service.getSaldo_CXP().subscribe(
+        rs => {this.global.saldo_cxp = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //COSTO VENTAS DEL PERIODO
+      this.service.getCostoVentas(this.fechaInicioP2,this.fechaFinP2).subscribe(
+        rs => {this.global.costoVentasP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //CXC DEL PERIODO
+      this.service.get_cxc_delperiodo(this.fechaInicioP2,this.fechaFinP2).subscribe(
+        rs => {this.global.saldo_cxc_P2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //CXP DEL PERIODO
+      this.service.get_cxp_periodo(this.fechaInicioP2,this.fechaFinP2).subscribe(
+        rs => {this.global.saldo_cxp_P2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //Funciones con dependencias de otras.
+      //---INVENTARIO INICIAL
+      //COMPRAS AL INICIO DEL PERIODO
+      this.service.get_compras(this.fechaInicioP2,this.fechaActual).subscribe(
+        rs => {this.global.compras_inicialP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //COSTO VENTAS INICIO DEL PERIODO
+      this.service.getCostoVentas(this.fechaInicioP2,this.fechaActual).subscribe(
+        rs => {this.global.costoVentas_inicialP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          //OBTENEMOS INVENTARIO INICIAL
+          this.global.inventarioInicialP2 = this.global.inventario - this.global.compras_inicialP2 + this.global.costoVentas_inicialP2;
+        });
+      //---INVENTARIO FINAL
+      //COMPRAS AL FINAL DEL PERIODO
+      this.service.get_compras(this.fechaFinP2,this.fechaActual).subscribe(
+        rs => {this.global.compras_finalP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      //COSTO VENTAS FINAL DEL PERIODO
+      this.service.getCostoVentas(this.fechaFinP2,this.fechaActual).subscribe(
+        rs => {this.global.costoVentas_finalP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          //OBTENEMOS INVENTARIO FINAL
+          this.global.inventarioFinalP2 = this.global.inventario - this.global.compras_finalP2 + this.global.costoVentas_finalP2;
+          //ASIGNAMOS ROTACION DE INVENTARIOS
+          this.global.rotacionInventarioP2 = this.global.costoVentasP2 / (this.global.inventarioInicialP2 + this.global.inventarioFinalP2)/2;
+        });
+      //---CXC INICIAL
+      this.service.get_cxc_delperiodo(this.fechaInicioP2,this.fechaActual).subscribe(
+        rs => {this.global.saldo_cxc_PeriodoInicialP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      
+      this.service.get_cxc_abonos_periodo(this.fechaInicioP2,this.fechaActual).subscribe(
+        rs => {this.global.abono_cxc_PeriodoInicialP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          this.global.saldo_cxc_inicial_P2 = this.global.saldo_cxc - this.global.saldo_cxc_PeriodoInicialP2 + this.global.abono_cxc_PeriodoInicialP2;
+        });
+      //---CXC FINAL
+      this.service.get_cxc_delperiodo(this.fechaFinP2,this.fechaActual).subscribe(
+        rs => {this.global.saldo_cxc_PeriodoFinalP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+      
+      this.service.get_cxc_abonos_periodo(this.fechaFinP2,this.fechaActual).subscribe(
+        rs => {this.global.abono_cxc_PeriodoFinalP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          this.global.saldo_cxc_final_P2 = this.global.saldo_cxc - this.global.saldo_cxc_PeriodoFinalP2 + this.global.abono_cxc_PeriodoFinalP2;
 
+          this.global.activoCorrienteP2 = this.global.efectivoP2 + this.global.inventarioFinalP2 + this.global.saldo_cxc_final_P2;
+
+          this.global.rotacionCarteraP2 = this.global.saldo_cxc_P2/(this.global.saldo_cxc_inicial_P2+this.global.saldo_cxc_final_P2)/2;
+        });
+
+      //---CXP INICIAL
+      this.service.get_cxp_periodo(this.fechaInicioP2,this.fechaActual).subscribe(
+        rs => {this.global.saldo_cxp_PeriodoInicialP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+
+      this.service.get_cxp_abonos_periodo(this.fechaInicioP2,this.fechaActual).subscribe(
+        rs => {this.global.abono_cxp_PeriodoInicialP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          this.global.saldo_cxp_inicial_P2 = this.global.saldo_cxp - this.global.saldo_cxp_PeriodoInicialP2 + this.global.abono_cxp_PeriodoInicialP2;
+        });
+      //---CXP FINAL
+      this.service.get_cxp_periodo(this.fechaFinP2,this.fechaActual).subscribe(
+        rs => {this.global.saldo_cxp_PeriodoFinalP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {});
+
+      this.service.get_cxp_abonos_periodo(this.fechaFinP2,this.fechaActual).subscribe(
+        rs => {this.global.abono_cxp_PeriodoFinalP2 = rs[0].valor;},
+        er =>console.log('Error: %s' , er),
+        () => {
+          this.global.saldo_cxp_final_P2 = this.global.saldo_cxp - this.global.saldo_cxp_PeriodoFinalP2 + this.global.abono_cxp_PeriodoFinalP2;
+
+          this.global.pasivoCorrienteP2 = this.global.saldo_cxp_final_P2;
+
+          //Variables de la vista
+          this.global.capitalNetoP2 = this.global.activoCorrienteP2 - this.global.pasivoCorrienteP2;
+
+          this.global.indiceSolvenciaP2 = this.global.activoCorrienteP2 / this.global.pasivoCorrienteP2;
+
+          this.global.pruebaAcidaP2 = (this.global.activoCorrienteP2 - this.global.inventario)/this.global.pasivoCorrienteP2;
+
+          this.global.rotacionCXP_P2 = this.global.saldo_cxp_P2 / (this.global.saldo_cxp_inicial_P2+this.global.saldo_cxp_final_P2)/2;
+        });
     }
 
   }
